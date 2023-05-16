7382eaa3c9260b644c344a00cb8c30820b8e68d4
TuShaR <tusharkaira456@gmail.com>
2023-05-16 15:30:41 +0530

    done

---
+++ src/Components/Demo.js
@@ -0,0 +1,40 @@
+const Demo = [
+    {
+        id:"1",
+        Name:"Rahul Mehra",
+        Email: "rahulmehra@gmail.com",
+        Mobile: "123456789"
+    },
+    {
+        id:"2",
+        Name:"Shubham Bisht",
+        Email: "shubhambisht@gmail.com",
+        Mobile: "123456789"
+    },
+    {
+        id:"3",
+        Name:"Suraj Adhikari",
+        Email: "surajadhikari@gmail.com",
+        Mobile: "123456789"
+    },
+    {
+        id:"4",
+        Name:"Amit Aswal",
+        Email: "amitaswal@gmail.com",
+        Mobile: "123456789"
+    },
+    {
+        id:"5",
+        Name:"Shubham Mehra",
+        Email: "shubhamehra@gmail.com",
+        Mobile: "123456789"
+    },
+    {
+        id:"6",
+        Name:"Umesh Dani",
+        Email: "umeshdani@gmail.com",
+        Mobile: "123456789"
+    }
+];
+
+export default Demo;
